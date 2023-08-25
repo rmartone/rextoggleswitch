e016adb03dffd58939275e0a38f5b0d3e4186ee5
rmartone <rmartone@gmail.com>
2023-08-25 18:22:53 -0400

    dead code

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,7 @@
 const path = require('path');
 const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 
 const projectMain = process.env.main;
-const assetsFolder = process.env.assets || './assets';
-const htmlTemplate = process.env.htmltemplate || './examples/index.tmpl';
 const usePluginTransformRuntime = process.env.usePluginTransformRuntime || false;
 
 var plugins = [];
@@ -36,33 +32,6 @@ module.exports = {
             "typeof WEBGL_DEBUG": JSON.stringify(false),
             WEBGL_DEBUG: JSON.stringify(false),
         }),
-        new HtmlWebpackPlugin({
-            filename: '../index.html',
-            template: htmlTemplate,
-            chunks: ['app'],
-            chunksSortMode: 'manual',
-            minify: {
-                removeAttributeQuotes: false,
-                collapseWhitespace: false,
-                html5: false,
-                minifyCSS: false,
-                minifyJS: false,
-                minifyURLs: false,
-                removeComments: false,
-                removeEmptyAttributes: false
-            },
-            hash: false
-        }),
-        new BrowserSyncPlugin({
-            host: process.env.IP || 'localhost',
-            port: process.env.PORT || 3000,
-            server: {
-                baseDir: './',
-                routes: {
-                    '/assets': assetsFolder,
-                }
-            },
-        })
     ],
     module: {
         rules: [
