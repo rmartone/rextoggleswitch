bffd7f8eed21ad9c6078d9a8107682eb432e744a
rmartone <rmartone@gmail.com>
2023-08-25 19:42:11 -0400

    removing instanceof test

--- plugins/utils/system/IsSceneObject.js
+++ plugins/utils/system/IsSceneObject.js
@@ -1,5 +1,6 @@
-const SceneClass = Phaser.Scene;
 var IsSceneObject = function (object) {
-    return (object instanceof SceneClass);
+    // return (object instanceof SceneClass);
+    // compatible with vite production builds
+    return obj && typeof obj === "object" && obj.hasOwnProperty("scene") && obj.scene !== undefined;
 }
 export default IsSceneObject;
