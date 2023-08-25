062da4b21b5cdbf57766293856e1ce2da54ad556
rmartone <rmartone@gmail.com>
2023-08-25 16:58:15 -0400

    dead code

--- plugins/logic/eventsheets/commandexecutor/methods/TreeManagerMethods.js
+++
@@ -1,8 +0,0 @@
-export default {
-    // TODO: More commands
-    set(config, manager) {
-        for (var name in config) {
-            manager.setData(name, config[name]);
-        }
-    },
-}
--- templates/ui/utils/build/CreateImage.js
+++
@@ -1,9 +0,0 @@
-import StatesImage from '../../statesimage/StatesImage.js';
-
-var CreateImage = function (scene, config) {
-    var gameObject = new StatesImage(scene, config);
-    scene.add.existing(gameObject);
-    return gameObject;
-}
-
-export default CreateImage;
